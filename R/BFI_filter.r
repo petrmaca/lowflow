054a6f18489377a636e24630151ab73397641fdc
petrmaca <maca@fzp.czu.cz>
2022-06-21 11:47:42 +0200

    filters and test.r added

--- R/BFI_filter.r
+++ R/BFI_filter.r
@@ -64,4 +64,98 @@ Eckhardt_filter = function(Q, a, BFI_max){
 #   return(b)
 # }
 
-
+baseflow_UKIH <- function(Q, endrule="NA"){
+  # R implementation of UKIH baseflow separation algorithm as
+  # described in Piggott et al. (2005)
+  #
+  # Inputs:
+  #   Q = discharge timeseries (no missing data) (any units are OK)
+  #   endrule = what to do with endpoints, which will always have NAs?
+  #     "NA" (default) = retain NAs
+  #     "Q" = use Q of the first/last point
+  #     "B" = use bf of the first/last point
+  #       
+  #
+  # Output:
+  #   bf = baseflow timeseries, same length and units as Q
+  
+  ## package dependencies
+  require(zoo)
+  require(dplyr)
+  require(magrittr)
+  
+  ## fixed interval of width 5
+  int_width <- 5
+  n.ints <- ceiling(length(Q)/int_width)
+  ints <- rep(seq(1,n.ints), each=int_width)[1:length(Q)]
+  
+  # build data frame
+  df <- data.frame(int = ints,
+                   day = seq(1,length(ints)),
+                   Q = Q)
+  
+  # summarize by interval
+  df <- 
+    df %>% 
+    group_by(int) %>% 
+    summarize(Qmin = min(Q),
+              n_int = sum(is.finite(int))) %>% 
+    left_join(df, ., by="int") %>% 
+    subset(n_int==int_width)
+  
+  # extract minimum Qmin for each interval; these are
+  # candidates to become turning points
+  df.mins <- df[df$Q==df$Qmin, ]
+  
+  # if there are two minima for an interval (e.g. two 
+  # days with same Q), choose the earlier one
+  df.mins <- df.mins[!duplicated(df.mins$int), ]
+  
+  ## determine turning points, defined as:
+  #    0.9*Qt < min(Qt-1, Qt+1)
+  # do this using a weighted rolling min function
+  df.mins$iQmin <- rollapply(df.mins$Qmin, width=3, align="center", 
+                             fill=NA, FUN=function(z) which.min(z*c(1,0.9,1)))
+  df.mins <- subset(df.mins, is.finite(iQmin))  # get rid of first/last point
+  TP.day <- df.mins$day[df.mins$iQmin==2]
+  TP.Qmin <- df.mins$Qmin[df.mins$iQmin==2]
+  
+  if (length(TP.day>1)){
+    
+    # linearly interpolate to length Q
+    bf <- rep(NaN, length(Q))
+    bf[TP.day] <- TP.Qmin
+    bf <- as.numeric(zoo::na.approx(bf, na.rm=F))
+    
+    # need to fill in NAs?
+    if (endrule=="Q"){
+      # start
+      bf[1:(TP.day[1]-1)] <- Q[1:(TP.day[1]-1)]
+      
+      # end
+      bf[(TP.day[length(TP.day)]+1):length(Q)] <- 
+        Q[(TP.day[length(TP.day)]+1):length(Q)]
+      
+    } else if (endrule=="B") {
+      # start
+      bf[1:(TP.day[1]-1)] <- bf[TP.day[1]]
+      
+      # end
+      bf[(TP.day[length(TP.day)]+1):length(Q)] <- 
+        bf[TP.day[length(TP.day)]]
+      
+    } else if (endrule != "NA") {
+      
+      stop("Invalid endrule")
+      
+    }
+    
+  } else {
+    bf <- rep(0, length(Q))
+  }
+  
+  # find any bf>Q and set to Q
+  i_tooHigh <- which(bf>Q)
+  bf[i_tooHigh] <- Q[i_tooHigh]
+  return(bf)
+}
---
+++ R/test.r
@@ -0,0 +1,38 @@
+library(lowflow)
+
+
+dta =read.table("/home/hubert/ownCloud/Shared/petr dp/discharge/daily/Q207290.txt",sep=";",skip=1)
+
+a=0.95
+ups =baseflow_UKIH(dta$V4)
+ups2=LH_filter(dta$V4,a)
+ups3=Chapman_filter(dta$V4,a)
+
+tres = baseflow_RecessionConstant(dta$V4)
+tres
+bfim=baseflow_BFImax(dta$V4,tres)
+ups4=Eckhardt_filter(dta$V4,a=a,BFI_max = bfim)
+
+
+plot(dta$V4, type="l")
+lines(ups,col="red")
+lines(ups2,col="blue")
+lines(ups3,col="green")
+lines(ups3,col="purple")
+
+
+
+library(devtools)
+install_github("cran/lfstat")
+library(lfstat)
+wmoBF <- baseflow(dta$V4)
+plot(dta$V4, type = "l")
+lines(wmoBF, col = 2)
+
+resDF= data.frame(WMO = wmoBF, ukih=ups,LH=ups2,CH =ups3,EK=ups4)
+plot(resDF)
+
+
+resRLM = select.recessionlimbs(dta$V4,minlength = 5)
+plot(dta$V4, type = "l")
+points(resRLM$t,resRLM$Q,col=2,pch=19)
