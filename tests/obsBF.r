a7d9a57aaa0fb66ae6a7c1ad4c4f821e0759315b
petrmaca <maca@fzp.czu.cz>
2022-06-29 12:40:53 +0200

    cyclic loading error

--- tests/obsBF.r
+++ tests/obsBF.r
@@ -28,7 +28,8 @@ for( i in 1:nrow(IDdta)){
   print(i)
 # dt=select.recessionlimbs(finDT$Rcur,as.character(finDT$DTM),minlength=9, begOUt = 3, endOUt=5)
   dta1b =dtaC[ID==IDdta$ID[i],]
-  dt=as.data.table(select.recessionlimbs(dta1b$R_mm_den,dta1b$DTM,minlength=9, begOUt = 5, endOUt=3))
+  # dt=as.data.table(select.recessionlimbs(dta1b$R_mm_den,dta1b$DTM,minlength=9, begOUt = 5, endOUt=3))
+  dt=as.data.table(select.recessionlimbs_Xie(dta1b$R_mm_den,dta1b$DTM,minlength=9, endOUt=3))
   dt[,OLA := unique(dta1b$OLA)]
   dt[,DBCN := unique(dta1b$DBCN)]
   dt[,UPOV_ID := unique(dta1b$UPOV_ID)]
@@ -38,4 +39,4 @@ obsDF <- rbindlist(obsDF)
 
 
 
-write_fst(obsDF,path="tests/data/obsRL_CenteredDIFFS_3begOUt_2endOut_Brutaaert.fst")
+write_fst(obsDF,path="tests/data/obsRL_CenteredDIFFS_3OR5begOUt_2endOut_Xie.fst")
